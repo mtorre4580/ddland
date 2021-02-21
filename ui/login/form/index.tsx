daa75cd6938bc05705cd7c06bd3e6c7d4d7044fa
mtorre <daniel.torre@mercadolibre.com>
2021-02-21 14:24:46 -0300

    refactor: prettier

--- ui/login/form/index.tsx
+++ ui/login/form/index.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import axios from 'axios';
-import { useRouter } from 'next/router'
+import { useRouter } from 'next/router';
 import styles from './form.module.scss';
 
 interface FormProps {
