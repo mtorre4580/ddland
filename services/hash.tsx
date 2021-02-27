7aff8371469e3579fb511038c07dc3fb6514f58a
mtorre <daniel.torre@mercadolibre.com>
2021-02-27 10:34:17 -0300

    chore: update checker

--- services/hash.tsx
+++ services/hash.tsx
@@ -1,7 +1,13 @@
 import bcrypt from 'bcryptjs';
 
+// Cost of salt
 const BCRYPT_SALT_ROUNDS = 12;
 
+/**
+ * Generate the hash for the current text
+ * @param text string
+ * @return Promise
+ */
 async function encrypt(text: string) {
   try {
     const salt = await bcrypt.genSalt(BCRYPT_SALT_ROUNDS);
@@ -9,15 +15,24 @@ async function encrypt(text: string) {
     return hash;
   } catch (err) {
     console.log('err', err);
+    throw new Error("Error al generar el hash");
+    
   }
 }
 
+/**
+ * Compare between two strings has same hash
+ * @param text string
+ * @param text2 string
+ * @return Promise
+ */
 async function compare(text: string, text2: string) {
   try {
     const status = await bcrypt.compare(text, text2);
     return status;
   } catch (err) {
     console.log('err', err);
+    throw new Error("Error al comparar textos");
   }
 }
 
