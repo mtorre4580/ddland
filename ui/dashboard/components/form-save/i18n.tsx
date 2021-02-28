54b6a69fb5604058c82ee4bbe4a5ced389d9859a
mtorre <daniel.torre@mercadolibre.com>
2021-02-28 19:52:59 -0300

    fix: title

--- ui/dashboard/components/form-save/i18n.tsx
+++ ui/dashboard/components/form-save/i18n.tsx
@@ -1,17 +1,17 @@
 export default {
   es: {
-    url: 'URL',
+    url: 'Path',
     title: 'Título',
-    hintURL: 'Define la ruta por cual vas a acceder https://ddland.vercel.app/{URL} (Sin la barra)',
+    hintURL: 'Define la ruta por cual vas a acceder https://ddland.vercel.app/{xxx} (Sin la barra)',
     hintTitle: 'Define el título que va tener tu página',
     urlExits: 'La URL ingresada ya existe, elige otra',
     save: 'Guardar',
   },
   en: {
-    url: 'URL',
+    url: 'Path',
     title: 'Title',
     hintTitle: 'Define the title that your page will have',
-    hintURL: 'Define the path through which you are going to access https://ddland.vercel.app/{URL} (Without the /)',
+    hintURL: 'Define the path through which you are going to access https://ddland.vercel.app/{xxx} (Without the /)',
     urlExits: 'The entered URL already exists, choose another',
     save: 'Save',
   },
