d56d27833e1bb51811e3537e6200432ac1e42221
mtorre <daniel.torre@mercadolibre.com>
2021-02-28 19:51:24 -0300

    fix: better experience url hint

--- ui/dashboard/components/form-save/i18n.tsx
+++ ui/dashboard/components/form-save/i18n.tsx
@@ -2,7 +2,7 @@ export default {
   es: {
     url: 'URL',
     title: 'Título',
-    hintURL: 'Define la dirección por la cual vas a acceder',
+    hintURL: 'Define la ruta por cual vas a acceder https://ddland.vercel.app/{URL} (Sin la barra)',
     hintTitle: 'Define el título que va tener tu página',
     urlExits: 'La URL ingresada ya existe, elige otra',
     save: 'Guardar',
@@ -11,7 +11,7 @@ export default {
     url: 'URL',
     title: 'Title',
     hintTitle: 'Define the title that your page will have',
-    hintURL: 'Define the address by which you are going to access',
+    hintURL: 'Define the path through which you are going to access https://ddland.vercel.app/{URL} (Without the /)',
     urlExits: 'The entered URL already exists, choose another',
     save: 'Save',
   },
