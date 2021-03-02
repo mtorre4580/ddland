d0420bf3770be04faa6c73c90f5b4adb0dca8d30
mtorre <daniel.torre@mercadolibre.com>
2021-03-02 10:54:33 -0300

    chore: add bundle size check

--- next.config.js
+++ next.config.js
@@ -1,7 +1,14 @@
+// Config to check bundle size
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+});
+
 // Config to i18n
-module.exports = {
+const config = {
   i18n: {
     locales: ['en', 'es'],
     defaultLocale: 'es',
   },
 };
+
+module.exports = withBundleAnalyzer(config);
