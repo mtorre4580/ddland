c24d179fd5ab06b04c7e970568b686364cc90162
mtorre <daniel.torre@mercadolibre.com>
2021-03-03 11:14:48 -0300

    chore: bootstrap cdn

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -16,6 +16,12 @@ function MyApp({ Component, pageProps }: AppProps) {
       <Head>
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link href="https://fonts.googleapis.com/css2?family=RocknRoll+One&display=swap" rel="stylesheet" />
+        <link
+          rel="stylesheet"
+          href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css"
+          integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l"
+          crossOrigin="anonymous"
+        />
       </Head>
       <I18nProvider locale={currentLocal}>
         <StateInspector name="DDland">
