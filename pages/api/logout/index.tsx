5dda4dd24d22b9b97c9cf8b0f4abd0cab190f9fb
mtorre4580 <mtorre4580@outlook.com>
2021-03-11 17:56:46 -0300

    fix: logout issue

--- pages/api/logout/index.tsx
+++ pages/api/logout/index.tsx
@@ -2,7 +2,8 @@ import { NextApiResponse } from 'next';
 import { NextApiRequestSession } from '../../../middlewares/auth';
 import withSession from '../../../middlewares/session';
 
-export default withSession((req: NextApiRequestSession, res: NextApiResponse) => {
-  req.session.destroy();
+export default withSession(async (req: NextApiRequestSession, res: NextApiResponse) => {
+  await req.session.destroy();
+  res.setHeader("cache-control", "no-store, max-age=0");
   res.status(200).json({ isLoggedIn: false });
 });
